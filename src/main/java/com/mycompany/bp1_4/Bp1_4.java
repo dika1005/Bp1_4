67e10fff96e4294dc78f1b390887fbb5b4dfe878
dika ramadani <dikabangkok21@gmail.com>
2024-11-11 23:04:13 +0700

    tugas

---
+++ src/main/java/com/mycompany/bp1_4/Bp1_4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bp1_4;
+
+/**
+ *
+ * @author Fujitsu U938
+ */
+public class Bp1_4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
